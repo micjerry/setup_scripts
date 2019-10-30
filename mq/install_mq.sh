5b38effe914617049cc88148dbb2dace48768b6d
micjerry <hansfei@126.com>
2019-10-30 17:33:37 +0800

    udpate mq install

--- mq/install_mq.sh
+++ mq/install_mq.sh
@@ -2,16 +2,32 @@
 
 LOCAL_IP="127.0.0.1"
 
+install_deps() {
+  sudo apt-get update -y
+  sudo apt-get install curl gnupg -y
+}
+
 setup_sign() {
-  wget -O - "https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc" | sudo apt-key add -
+  #wget -O - "https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc" | sudo apt-key add -
   
-  echo "deb https://dl.bintray.com/rabbitmq-erlang/debian" > /etc/apt/sources.list.d/bintray.rabbitmq.list
-  echo "deb https://dl.bintray.com/rabbitmq/debian bionic main" >> /etc/apt/sources.list.d/bintray.rabbitmq.list
+  #echo "deb https://dl.bintray.com/rabbitmq-erlang/debian" > /etc/apt/sources.list.d/bintray.rabbitmq.list
+  #echo "deb https://dl.bintray.com/rabbitmq/debian bionic main" >> /etc/apt/sources.list.d/bintray.rabbitmq.list
+  
+  curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | sudo apt-key add -
+  sudo apt-get install apt-transport-https
+  
+sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
+deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang-21.x
+deb https://dl.bintray.com/rabbitmq/debian bionic main
+EOF
 }
 
 setup() {
-  apt-get update
-  apt-get -y install rabbitmq-server
+  #apt-get update
+  #apt-get -y install rabbitmq-server
+  #rabbitmq-plugins enable rabbitmq_management
+  sudo apt-get update -y
+  sudo apt-get install rabbitmq-server -y --fix-missing
   rabbitmq-plugins enable rabbitmq_management
   
 cat << EOT > /etc/rabbitmq/rabbitmq.config
@@ -49,6 +65,7 @@ config() {
   /usr/sbin/rabbitmqctl set_permissions -p vhostcdr lgadmin ".*" ".*" ".*"
 }
 
+install_deps
 setup_sign
 setup
 config
