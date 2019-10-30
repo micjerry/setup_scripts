e00cc3d1a9713c2d598eb417893c7a83c17e9495
micjerry <hansfei@126.com>
2019-10-30 16:13:48 +0800

    add mysql install

---
+++ db/create_swcdr.sql
@@ -0,0 +1,59 @@
+--
+-- Table structure for table `callhistory`
+--
+
+DROP TABLE IF EXISTS `callhistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `callhistory` (
+  `callid` varchar(32) NOT NULL,
+  `groupid` varchar(32) NOT NULL,
+  `dialogid` varchar(32) NOT NULL,
+  `userid` varchar(32) NOT NULL,
+  `robotid` varchar(32) NOT NULL,  
+  `caller` varchar(32),
+  `callee` varchar(32) NOT NULL, 
+  `gateway` varchar(32),
+  `createtime` int(10) DEFAULT NULL,
+  `answertime` int(10) DEFAULT NULL,
+  `hangtime` int(10) DEFAULT NULL,
+  `callduration` int(10) DEFAULT NULL,
+  `hangcause` int(10) DEFAULT NULL,
+  PRIMARY KEY (`callid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;  
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `calldialog`
+--
+
+DROP TABLE IF EXISTS `calldialog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calldialog` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `callid` varchar(32) NOT NULL,
+  `dialogseq` int(10) NOT NULL,
+  `playfile` varchar(32),  
+  `detected` varchar(512),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;  
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `callrecord`
+--
+
+DROP TABLE IF EXISTS `callrecord`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `callrecord` (
+  `callid` varchar(32) NOT NULL,
+  `recordfile` varchar(32) NOT NULL,
+  `recordhost` varchar(32) NOT NULL,  
+  `recordlen` int(10) NOT NULL,
+  `cloudlk` varchar(128),
+  PRIMARY KEY (`callid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;  
+/*!40101 SET character_set_client = @saved_cs_client */;
+
---
+++ mysql/install_msyql.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+MYSQL_NEW_DBPATH="/opt/mysqldb/"
+MYSQL_DEFAULT_DBPATH="/var/lib/mysql/"
+MYSQL_SEC="ak2s098913"
+
+MTSQL_PIDPATH="/var/run/mysqld"
+MYSQL_ELOG="/var/log/mysql/error.log"
+
+cat << EOF > mysql.users
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_SEC}';
+GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY '${MYSQL_SEC}';
+FLUSH PRIVILEGES;
+EOF
+
+die() {
+  echo "${red}$1${reset}"
+  exit 1
+}
+
+install_mysql() {
+  [ -f ${MYSQL_ELOG} ] && rm -f ${MYSQL_ELOG}
+  apt-get -y install mysql-server
+}
+
+initial_mysql() {
+  #change mysql db path in conf
+  sed -i "s#datadir\(.*\)#datadir         =${MYSQL_NEW_DBPATH}#" /etc/mysql/mysql.conf.d/mysqld.cnf
+  
+  rm -rf ${MYSQL_NEW_DBPATH}
+  mkdir -p ${MYSQL_NEW_DBPATH}
+  chmod 755 ${MYSQL_NEW_DBPATH}
+  chown mysql:mysql ${MYSQL_NEW_DBPATH}
+  
+  rm -rf ${MTSQL_PIDPATH}
+  mkdir -p ${MTSQL_PIDPATH}
+  chmod 755 ${MTSQL_PIDPATH}
+  chown mysql:mysql ${MTSQL_PIDPATH}
+  
+  #change mysql db path in apparmor
+  if [ -f /etc/apparmor.d/usr.sbin.mysqld ]; then
+    sed -i "s_${MYSQL_DEFAULT_DBPATH}_${MYSQL_NEW_DBPATH}_g" /etc/apparmor.d/usr.sbin.mysqld
+    service apparmor restart
+  fi
+  
+  #initial mysql
+  /usr/sbin/mysqld --initialize
+  
+  systemctl start mysql.service
+}
+
+config_mysql() {
+  sleep 5
+  local temp_se=`grep 'temporary password' ${MYSQL_ELOG} | awk -F 'localhost: ' '{print $2}'`
+  [ -z "${temp_se}" ] && die "no temporary password found."
+  mysql -uroot --password="${temp_se}" --connect-expired-password < mysql.users
+  rm -f mysql.users
+}
+
+initial_dbs() {
+  for filename in $(find . -name "create_*.sql"); do
+    local dbfile_name=$(basename "$filename")
+    local db_name=$(echo ${dbfile_name} | awk -F_ '{print $2}' | awk -F. '{print $1}')
+    [ -z "${db_name}" ] && continue
+    
+    echo "CREATE DATABASE ${db_name} CHARACTER SET utf8 COLLATE utf8_general_ci; " | mysql -uroot --password=${MYSQL_SEC}
+    mysql -u root -p${MYSQL_SEC} ${db_name} < ${filename}
+  done
+}
+
+install_mysql
+initial_mysql
+config_mysql
+initial_dbs
