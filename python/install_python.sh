e834d923dc2ca1ca0085efdfeab0ea8809108582
micjerry <hansfei@126.com>
2019-11-04 15:20:32 +0800

    add python

--- python/install_python.sh
+++ python/install_python.sh
@@ -3,6 +3,8 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
+CUR_PATH=`pwd`
+
 die() {
   echo "${red}$1${reset}"
   exit 1
@@ -11,51 +13,44 @@ die() {
 install_pyenv() {
   sudo apt-get install -y build-essential libbz2-dev libssl-dev libreadline-dev libsqlite3-dev tk-dev
   sudo apt-get install -y libpng-dev libfreetype6-dev
-  curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+  sudo apt-get install -y libffi-dev
+  
+  [ -d /root/.pyenv ]
+  mkdir -p /root/.pyenv
+  tar -xzvf pyenv-1.2.14.tar.gz -C /root/.pyenv
+  mv -rf /root/.pyenv/pyenv-1.2.14/* /root/.pyenv
+  rm -rf /root/.pyenv/pyenv-1.2.14
+  sed -i "/PYENV_ROOT/d" ~/.bashrc
+  sed -i "/pyenv init/d" ~/.bashrc
+  echo "export PYENV_ROOT=\"\$HOME/.pyenv\"" >> ~/.bashrc
+  echo "export PATH=\"\$PYENV_ROOT/bin:\$PATH\"" >> ~/.bashrc
+  echo "eval \"\$(pyenv init -)\"" >> ~/.bashrc
+  source ~/.bashrc
 }
 
 install_python() {
-  sudo apt update
-  sudo apt -y install software-properties-common
-  sudo add-apt-repository -y ppa:deadsnakes/ppa
-  sudo apt -y install python3.7
+  export PYTHON_BUILD_CACHE_PATH=${CUR_PATH}
+  pyenv install 3.8.0
+  pyenv global 3.8.0
+  pyenv rehash
+  python -m pip install --upgrade pip
 }
 
 install_packages() {
-
+  #python -m pip install --no-index --find-links=pypackages tornado==6.0.3
+  python -m pip install tornado
+  python -m pip install redis
+  python -m pip install redis-py-cluster
+  python -m pip install python-redis
+  
+  [ -f Tornado-MySQL-0.5.tar.gz ] || die "Tornado-MySQL-0.5.tar.gz does not exist"
+  tar -xzvf Tornado-MySQL-0.5.tar.gz -C ${CUR_PATH}
+  cd  Tornado-MySQL-0.5
+  python setup.py install
+  cd - 
+  rm -rf Tornado-MySQL-0.5
 }
 
-config_nginx() {
-
-}
-
-usage()
-{
-  echo "Usage: $0 -h bind_ip_addr"
-  exit 0
-}
-
-while [[ $# -gt 1 ]]
-do
-  key="$1"
-  case $key in
-    -h|--host)
-      BIND_IPADDR="$2"
-      shift
-      ;;
-    -h|--help)
-      usage
-      ;;
-    *)
-      echo "invalid arguments"
-      usage
-      ;;
-  esac
-  shift 
-done
-
-[ -z "${BIND_IPADDR}" ] && usage
-
-install_deps
-install_nginx
-config_nginx
+install_pyenv
+install_python
+install_packages
